5773b4f11ec17fa2e324cc8465f2be736583d00c
balin-sv <balinsvet@gmail.com>
2022-02-07 10:39:18 -0300

    test

---
+++ build/precache-manifest.f1c24139b9d14366ebc98e338e5d164b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c89b9ad4c8b1c0c5e87",
+    "url": "/static/css/main.4fecc813.chunk.css"
+  },
+  {
+    "revision": "0c89b9ad4c8b1c0c5e87",
+    "url": "/static/js/main.ddcaa93b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6270547d1f5f154219a3",
+    "url": "/static/js/2.a18eef99.chunk.js"
+  },
+  {
+    "revision": "00e75119788da85cada09ed07154d5cd",
+    "url": "/index.html"
+  }
+];
